71008ae6c4a15eb9167b82d28dc1b96283987c08
Camille Linan <camille.linan@hotmail.com>
2022-10-11 00:11:41 +0200

    add-header

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import Header from "../components/Layout/Header";
+import Intro from "../components/Layout/Intro";
 
 const Home = () => {
     return (
         <>
             <Header />
+            <Intro />
         </>
     )
 }
