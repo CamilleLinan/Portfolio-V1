3625a13148785b45feff69338a02e509fdef2510
Camille Linan <camille.linan@hotmail.com>
2022-10-11 23:59:17 +0200

    add-cards OK

--- src/datas/projectList.js
+++ src/datas/projectList.js
@@ -1,5 +1,9 @@
-import booki from './cover/booki-cover1.png'
-import ohmyfood from './cover/ohmyfood-cover1.png'
+import booki from './cover/1booki-cover.png'
+import ohmyfood from './cover/2ohmyfood-cover.png'
+import lapanthere from './cover/3lapanthere-cover.png'
+import hottakes from './cover/4hottakes-cover.png'
+import kanap from './cover/5kanap-cover.png'
+import groupomania from './cover/6groupomania-cover.png'
 
 export const projectList = [
     {
@@ -12,24 +16,24 @@ export const projectList = [
         name: 'OhMyFood',
         cover: ohmyfood,
     },
-    // {
-    //     id: '03',
-    //     name: 'La Panthere',
-    //     img: '',
-    // },
-    // {
-    //     id: '04',
-    //     name: 'Kanap',
-    //     img: '',
-    // },
-    // {
-    //     id: '05',
-    //     name: 'Hot Takes',
-    //     img: '',
-    // },
-    // {
-    //     id: '06',
-    //     name: 'Groupomania',
-    //     img: '',
-    // }
+    {
+        id: '03',
+        name: 'La Panthere',
+        cover: lapanthere,
+    },
+    {
+        id: '04',
+        name: 'Kanap',
+        cover: hottakes,
+    },
+    {
+        id: '05',
+        name: 'Hot Takes',
+        cover: kanap,
+    },
+    {
+        id: '06',
+        name: 'Groupomania',
+        cover: groupomania,
+    }
 ]
