9f02adff8da5012cb599be96ae729f8669ce922b
Camille Linan <camille.linan@hotmail.com>
2022-10-12 18:34:34 +0200

    add scss for cards

--- src/datas/projectList.jsx
+++ src/datas/projectList.jsx
@@ -11,35 +11,41 @@ export const projectList = [
         name: 'Booki',
         cover: booki,
         alt: 'booki-cover',
+        resume: 'Intégration de maquette en HTML et CSS'
     },
     {
         id: '02',
         name: 'OhMyFood',
         cover: ohmyfood,
         alt: 'ohmyfood-cover',
+        resume: 'Intégration de maquette en HTML et animation avec SASS'
     },
     {
         id: '03',
         name: 'La Panthere',
         cover: lapanthere,
         alt: 'lapanthere-cover',
+        resume: 'Refonte de site web et SEO'
     },
     {
         id: '04',
         name: 'Kanap',
         cover: kanap,
         alt: 'kanap-cover',
+        resume: 'Intégration d\'une API en JavaScript'
     },
     {
         id: '05',
         name: 'Hot Takes',
         cover: hottakes,
         alt: 'hottakes-cover',
+        resume: 'Création d\'un serveur et d\'une API avec Node, Express et MongoDB'
     },
     {
         id: '06',
         name: 'Groupomania',
         cover: groupomania,
         alt: 'groupomania-cover',
+        resume: 'Création d\'un réseau social d\'entreprise avec React.js' 
     }
 ]
