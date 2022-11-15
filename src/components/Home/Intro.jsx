ffc1f91b2faafb316af4d5d07f38e024fe8b7953
Camille Linan <camille.linan@hotmail.com>
2022-11-15 22:33:02 +0100

    corrections

--- src/components/Home/Intro.jsx
+++ src/components/Home/Intro.jsx
@@ -9,7 +9,7 @@ const Intro = () => {
                 <p className="intro_titles_subtitle">
                     Après avoir travaillé 7 ans dans le secteur de la vente, 
                     j'ai choisi de me reconvertir dans un domaine qui me passionne :
-                    le développement web, et plus particulièrement l'UX design.
+                    le développement web, et plus particulièrement l'UI/UX design.
                 </p>
                 <p className="intro_titles_subtitle">
                     Vous pouvez consulter ci-dessous mes projets réalisés avec OpenClassRooms, bonne visite !
