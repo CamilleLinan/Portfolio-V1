8aefecad88677912ed813e7d425394fad08714aa
Camille Linan <camille.linan@hotmail.com>
2022-12-02 23:57:22 +0100

    refacto projectList

--- src/components/ProjectPage/DisplayContentProject.jsx
+++ src/components/ProjectPage/DisplayContentProject.jsx
@@ -24,76 +24,55 @@ const DisplayContentProject = () => {
                         </article>
                         <article className='project_page_pres_article'>
                             <h2 className='project_page_pres_title'>Langages utilisés :</h2>
+                            
                             <div className='project_page_pres_logos'>
-                                {project.html && <>
-                                    <img src={project.html} alt='logo HTML' className='project_page_pres_logos_logo' />
-                                </>}
-                                {project.css && <>
-                                    <img src={project.css} alt='logo CSS' className='project_page_pres_logos_logo' />
-                                </>}
-                                {project.sass && <>
-                                    <img src={project.sass} alt='logo SASS' className='project_page_pres_logos_logo' />
-                                </>}
-                                {project.js && <>
-                                    <img src={project.js} alt='logo JavaScript' className='project_page_pres_logos_logo' />
-                                </>}
-                                {project.react && <>
-                                    <img src={project.react} alt='logo React' className='project_page_pres_logos_logo' />
-                                </>}
-                                {project.node && <>
-                                    <img src={project.node} alt='logo Node.js' className='project_page_pres_logos_logo' />
-                                </>}
-                                {project.mongo && <>
-                                    <img src={project.mongo} alt='logo MongoDB' className='project_page_pres_logos_logo' />
-                                </>}
+                                {project.languages.map((logo, i) => (
+                                    <img src={logo} alt='logo HTML' className='project_page_pres_logos_logo' />
+                                ))}
                             </div>
                         </article>
                     </section>
                     
                     <section className='project_page_cover'>
-                        {project.imgDesk && project.imgTab && project.imgMobile && project.imgColors && project.imgIcons && <>
+                        
+                        {project.picturesPres && <>
                             <h3 className='project_page_cover_title'>Version desktop, tablette et mobile :</h3>
                             <article className='project_page_cover_imgs'>
-                                <img src={project.imgDesk} alt={project.altDesk} className='project_page_cover_img project_page_cover_img_1' />
-                                <img src={project.imgTab} alt={project.altTab} className='project_page_cover_img' />
-                                <img src={project.imgMobile} alt={project.altMobile} className='project_page_cover_img' />
+                                {project.picturesPres.map((picturePres, i) => (
+                                    <img src={picturePres} alt={`cover ${project.name} openclassrooms desktop, tablette et mobile`} className='project_page_cover_img project_page_cover_img_1' />
+                                ))}
                             </article>
                             
                             <h3 className='project_page_cover_title'>Identité graphique :</h3>
                             <article className='project_page_ui'>
-                                <img src={project.imgColors} alt={project.altColors} className='project_page_ui_img project_page_ui_img_colors' />
-                                <img src={project.imgIcons} alt={project.altIcons} className='project_page_ui_img project_page_ui_img_icons' />
+                                {project.graphicId.map((gId, i) => (
+                                    <img src={gId} alt={`couleurs et icons de ${project.name} openclassrooms`} className='project_page_ui_img project_page_ui_img_colors' />
+                                ))}
                             </article>
                         </>}
                         
                         {project.imgBefore && project.imgAfter && <>
                             <h3 className='project_page_cover_title'>Version tablette, avant et après :</h3>
                             <article className='project_page_cover_imgs'>
-                                    <img src={project.imgBefore} alt={project.altBefore} className='project_page_cover_img_fulltab project_page_cover_img_fulltab_1' />
-                                    <img src={project.imgAfter} alt={project.altAfter} className='project_page_cover_img_fulltab' />
+                                    <img src={project.imgBefore} alt={`cover ${project.name} openclassrooms avant`} className='project_page_cover_img_fulltab project_page_cover_img_fulltab_1' />
+                                    <img src={project.imgAfter} alt={`cover ${project.name} openclassrooms après`} className='project_page_cover_img_fulltab' />
                             </article>
                         </>}
 
-                        {project.imgEtap1 && project.imgEtap2 && <>
+                        {project.picturesEtap && <>
                             <h3 className='project_page_cover_title'>Parcours utilisateur :</h3>
                             <article className='project_page_cover_imgs'>
-                                <img src={project.imgEtap1} alt={project.altEtap1} className='project_page_cover_img project_page_cover_img_1 project_page_cover_img_etap' />
-                                <img src={project.imgEtap2} alt={project.altEtap2} className='project_page_cover_img project_page_cover_img_etap' />
-                                {project.imgEtap3 && project.imgEtap4 && <>
-                                    <img src={project.imgEtap3} alt={project.altEtap3} className='project_page_cover_img project_page_cover_img_etap' />
-                                    <img src={project.imgEtap4} alt={project.altEtap4} className='project_page_cover_img project_page_cover_img_etap' />
-                                </>}
-                                {project.imgEtap5 && project.imgEtap6 && <>
-                                    <img src={project.imgEtap5} alt={project.altEtap5} className='project_page_cover_img project_page_cover_img_etap' />
-                                    <img src={project.imgEtap6} alt={project.altEtap6} className='project_page_cover_img project_page_cover_img_etap' />
-                                </>}
+                                {project.picturesEtap.map((pictureEtap, i) => (
+                                    <img src={pictureEtap} alt={`parcours utilisateur ${project.name} openclassrooms`} className='project_page_cover_img project_page_cover_img_1 project_page_cover_img_etap' />
+                                ))}
                             </article>
 
-                            {project.imgColors && project.imgIcons && <>
+                            {project.graphicId && <>
                                 <h3 className='project_page_cover_title'>Identité graphique :</h3>
                                 <article className='project_page_ui'>
-                                    <img src={project.imgColors} alt={project.altColors} className='project_page_ui_img project_page_ui_img_colors' />
-                                    <img src={project.imgIcons} alt={project.altIcons} className='project_page_ui_img project_page_ui_img_icons' />
+                                    {project.graphicId.map((gId, i) => (
+                                        <img src={gId} alt={`couleurs et icons de ${project.name} openclassrooms`} className='project_page_ui_img project_page_ui_img_colors' />
+                                    ))}
                                 </article>
                             </>}
                         </>}
--- src/datas/projectsList.jsx
+++ src/datas/projectsList.jsx
@@ -8,7 +8,7 @@ import logoMongo from '../styles/img/logos/logo-mongo.png'
 import logoReact from '../styles/img/logos/logo-react.png'
 
 // BOOKI
-import booki from './cover/1Booki/bookiCover.webp'
+import bookiCover from './cover/1Booki/bookiCover.webp'
 import bookiDesk from './cover/1Booki/bookiDesk.webp'
 import bookiTab from './cover/1Booki/bookiTab.webp'
 import bookiMobile from './cover/1Booki/bookiMobile.webp'
@@ -16,7 +16,7 @@ import bookiColors from './cover/1Booki/bookiColors.png'
 import bookiIcons from './cover/1Booki/bookiIcons.png'
 
 // OHMYFOOD
-import ohmyfood from './cover/2Ohmyfood/ohmyfoodCover.webp'
+import ohmyfoodCover from './cover/2Ohmyfood/ohmyfoodCover.webp'
 import ohmyfoodDesk from './cover/2Ohmyfood/ohmyfoodDesk.webp'
 import ohmyfoodTab from './cover/2Ohmyfood/ohmyfoodTab.webp'
 import ohmyfoodMobile from './cover/2Ohmyfood/ohmyfoodMobile.webp'
@@ -24,24 +24,24 @@ import ohmyfoodColors from './cover/2Ohmyfood/ohmyfoodColors.png'
 import ohmyfoodIcons from './cover/2Ohmyfood/ohmyfoodIcons.png'
 
 // LAPANTHERE
-import lapanthere from './cover/3LaPanthere/lapanthereCover.webp'
+import lapanthereCover from './cover/3LaPanthere/lapanthereCover.webp'
 import lapanthereBefore from './cover/3LaPanthere/lapanthereBefore.webp'
 import lapanthereAfter from './cover/3LaPanthere/lapanthereAfter.webp'
 
 // KANAP
-import kanap from './cover/4Kanap/kanapCover.webp'
+import kanapCover from './cover/4Kanap/kanapCover.webp'
 import kanapEtap1 from './cover/4Kanap/kanapEtap1.webp'
 import kanapEtap2 from './cover/4Kanap/kanapEtap2.webp'
 import kanapEtap3 from './cover/4Kanap/kanapEtap3.webp'
 import kanapEtap4 from './cover/4Kanap/kanapEtap4.webp'
 
 // HOTTAKES
-import hottakes from './cover/5Hottakes/hottakesCover.webp'
+import hottakesCover from './cover/5Hottakes/hottakesCover.webp'
 import hottakesEtap1 from './cover/5Hottakes/hottakesEtap1.webp'
 import hottakesEtap2 from './cover/5Hottakes/hottakesEtap2.webp'
 
 // GROUPOMANIA
-import groupomania from './cover/6Groupomania/groupomaniaCover.webp'
+import groupoCover from './cover/6Groupomania/groupoCover.webp'
 import groupoEtap1 from './cover/6Groupomania/groupoEtap1.webp'
 import groupoEtap2 from './cover/6Groupomania/groupoEtap2.webp'
 import groupoEtap3 from './cover/6Groupomania/groupoEtap3.webp'
@@ -52,7 +52,7 @@ import groupoColors from './cover/6Groupomania/groupoColors.png'
 import groupoIcons from './cover/6Groupomania/groupoIcons.png'
 
 // 7 KASA
-import kasa from './cover/7Kasa/kasaCover.webp'
+import kasaCover from './cover/7Kasa/kasaCover.webp'
 import kasaEtap1 from './cover/7Kasa/kasaEtap1.webp'
 import kasaEtap2 from './cover/7Kasa/kasaEtap2.webp'
 import kasaEtap3 from './cover/7Kasa/kasaEtap3.webp'
@@ -65,162 +65,93 @@ export const projectsList = [
     {
         id: '07',
         name: 'Kasa',
-        cover: kasa,
+        cover: kasaCover,
         alt: 'cover kasa openclassrooms',
         resume: 'Création d\'une application web de location immobilière avec React',
         context: 'Kasa est dans le métier de la location d’appartements entre particuliers depuis près de 10 ans. L\'entreprise souhaite faire une refonte totale de son site web.',
         objectifs: 'Développer l\'ensemble de l\'application et ses composants avec React, en suivant les maquettes Figma fournies. Des composants réutilisables doivent être créés pour les onglets ouvrants et le carrousel de photo.',
-        html: logoHtml,
-        css: logoCss,
-        sass: logoSass,
-        js: logoJS,
-        react: logoReact,
-        imgEtap1: kasaEtap1,
-        altEtap1: 'cover kasa openclassrooms etape 1',
-        imgEtap2: kasaEtap2,
-        altEtap2: 'cover kasa openclassrooms etape 2',
-        imgEtap3: kasaEtap3,
-        altEtap3: 'cover kasa openclassrooms etape 3',
-        imgEtap4: kasaEtap4,
-        altEtap4: 'cover kasa openclassrooms etape 4',
-        imgEtap5: kasaEtap5,
-        altEtap5: 'cover kasa openclassrooms etape 5',
-        imgEtap6: kasaEtap6,
-        altEtap6: 'cover kasa openclassrooms etape 6',
+        languages: [logoHtml, logoCss, logoSass, logoJS, logoReact],
+        picturesEtap: [kasaEtap1, kasaEtap2, kasaEtap3, kasaEtap4, kasaEtap5, kasaEtap6],
         gitLink: 'https://github.com/CamilleLinan/Projet-8-Kasa',
         webSite: 'https://projet-8-kasa-brnf30w8b-camillelinan.vercel.app/'
     },
     {
         id: '06',
         name: 'Groupomania',
-        cover: groupomania,
+        cover: groupoCover,
         alt: 'cover groupomania openclassrooms',
         resume: 'Création d\'un réseau social d\'entreprise avec React.js',
         context: 'Groupomania est une entreprise ayant un ralentissement de sa productivité attribué à une baisse de la motivation et de l\'implication des employés. Elle souhaite donc créer un réseau social d\'entreprise afin d\'améliorer la relation entre ces derniers.',
         objectifs: 'Créer une partie front-end avec un framework JavaScript, ici React. Et une API sécurisée permettant aux utilisateurs de se créer un compte, se connecter et se déconnecter. Ainsi que créer un post, modifier ou supprimer leur post, et liker tous les posts. J\'ai également ajouté une page où les utilisateurs peuvent modifier leurs informations. ',
-        html: logoHtml,
-        css: logoCss,
-        sass: logoSass,
-        js: logoJS,
-        node: logoNode,
-        mongo: logoMongo,
-        react: logoReact,
-        imgEtap1: groupoEtap1,
-        altEtap1: 'cover groupomania openclassrooms etape 1',
-        imgEtap2: groupoEtap2,
-        altEtap2: 'cover groupomania openclassrooms etape 2',
-        imgEtap3: groupoEtap3,
-        altEtap3: 'cover groupomania openclassrooms etape 3',
-        imgEtap4: groupoEtap4,
-        altEtap4: 'cover groupomania openclassrooms etape 4',
-        imgEtap5: groupoEtap5,
-        altEtap5: 'cover groupomania openclassrooms etape 5',
-        imgEtap6: groupoEtap6,
-        altEtap6: 'cover groupomania openclassrooms etape 6',
-        imgColors: groupoColors,
-        altColors: 'groupomania openclassrooms colors',
-        imgIcons: groupoIcons,
-        altIcons: 'groupomania openclassrooms icons',
+        languages: [logoHtml, logoCss, logoSass, logoJS, logoNode, logoMongo, logoReact],
+        picturesEtap: [groupoEtap1, groupoEtap2, groupoEtap3, groupoEtap4, groupoEtap5, groupoEtap6],
+        graphicId: [groupoColors, groupoIcons],
         gitLinkBack: 'https://github.com/CamilleLinan/Projet-7-Groupomania-Back',
         gitLinkFront: 'https://github.com/CamilleLinan/Projet-7-Groupomania-Front'  
     },
     {
         id: '05',
         name: 'Hot Takes',
-        cover: hottakes,
+        cover: hottakesCover,
         alt: 'cover hottakes piiquante openclassrooms',
         resume: 'Création d\'un serveur et d\'une API avec Node, Express et MongoDB',
         context: 'Les sauces piquantes sont de plus en plus populaires, c\'est pourquoi la marque de condiments à base de piment Piiquante souhaite développer une application web de critique des sauces piquantes appelée "Hot Takes".',
         objectifs: 'Construire une API sécurisée avec ses models, controllers et routes. Les utilisateurs de l\'application doivent pouvoir créer de nouvelles sauces, modifier ou supprimer leur sauce, ainsi que liker ou disliker toutes les sauces créées.',
-        node: logoNode,
-        mongo: logoMongo,
-        imgEtap1: hottakesEtap1,
-        altEtap1: 'cover hot takes openclassrooms etape 1',
-        imgEtap2: hottakesEtap2,
-        altEtap2: 'cover hot takes openclassrooms etape 2',
+        languages: [logoNode, logoMongo],
+        picturesEtap: [hottakesEtap1, hottakesEtap2],
         gitLink: 'https://github.com/CamilleLinan/Projet-6-Piiquante-Back'
     },
     {
         id: '04',
         name: 'Kanap',
-        cover: kanap,
+        cover: kanapCover,
         alt: 'cover kanap openclassrooms',
         resume: 'Intégration d\'une API en JavaScript',
         context: 'Kanap est une marque de canapés qui vend ses produits depuis sa boutique exclusivement. Aujourd’hui, celle-ci souhaiterait avoir une plateforme de e-commerce en plus de sa boutique physique pour vendre ses produits sur Internet.',
         objectifs: 'Unifier les travaux déjà réalisés par les développeurs front-end et back-end, en intégrant dynamiquement les éléments de l\'API dans les différentes pages web avec JavaScript. L\'utilisateur doit pouvoir visualiser l\'ensemble des produits disponibles, ajouter un ou plusieurs article dans un panier, modifier ou supprimer son panier et enfin, passer commande.',
-        html: logoHtml,
-        js: logoJS,
-        imgEtap1: kanapEtap1,
-        altEtap1: 'cover kanap openclassrooms etape 1',
-        imgEtap2: kanapEtap2,
-        altEtap2: 'cover kanap openclassrooms etape 2',
-        imgEtap3: kanapEtap3,
-        altEtap3: 'cover kanap openclassrooms etape 3',
-        imgEtap4: kanapEtap4,
-        altEtap4: 'cover kanap openclassrooms etape 4',
+        languages: [logoHtml, logoJS],
+        picturesEtap: [kanapEtap1, kanapEtap2, kanapEtap3, kanapEtap4],
         gitLink: 'https://github.com/CamilleLinan/Projet-5-Kanap'
     },
     {
         id: '03',
         name: 'La Panthere',
-        cover: lapanthere,
+        cover: lapanthereCover,
         alt: 'cover lapanthere openclassrooms',
         resume: 'Refonte de site web et SEO',
         context: 'La Panthère est une grande agence de web design basée à Lyon. L\'activité de l\'entreprise est en perte de vitesse et son site web n\'apparaît pas en première page des moteurs de recherche.',
         objectifs: 'Analyser l\'état actuel de SEO du site fourni et améliorer les points les plus importants concernant notamment la vitesse, la taille et l\'accessibilité du site. Puis créer un rapport d\'optimisations pour comparer les résultats avant/après fournis par différents analyseurs de qualité et performance web.',
-        html: logoHtml,
-        css: logoCss,
+        languages: [logoHtml, logoCss],
         imgBefore: lapanthereBefore,
-        altBefore: 'cover la panthere openclassrooms avant',
         imgAfter: lapanthereAfter,
-        altAfter: 'cover la panthere openclassrooms après',
         gitLink: 'https://github.com/CamilleLinan/Projet-4-La-Panthere',
         webSite: 'https://camillelinan.github.io/Projet-4-La-Panthere/'
     },
     {
         id: '02',
         name: 'OhMyFood',
-        cover: ohmyfood,
+        cover: ohmyfoodCover,
         alt: 'cover ohmyfood openclassrooms',
         resume: 'Intégration de maquette en HTML et animation avec SASS',
         context: 'Ohmyfood! est une jeune startup voulant s\'imposer sur le marché de la restauration. En plus des systèmes classiques de réservation, les clients pourront composer le menu de leur repas pour que les plats soient prêts à leur arrivée.',
         objectifs: 'Intégrer la maquette créée par le designer UI en mobilefirst en respectant les contraintes graphiques et d\'animations, sans utiliser de JavaScript. Mise en page libre pour les versions desktop et tablette.',
-        html: logoHtml,
-        css: logoCss,
-        sass: logoSass,
-        imgDesk: ohmyfoodDesk,
-        altDesk: 'cover ohmyfood openclassrooms desktop',
-        imgTab: ohmyfoodTab,
-        altTab: 'cover ohmyfood openclassrooms tablette',
-        imgMobile: ohmyfoodMobile,
-        altMobile: 'cover ohmyfood openclassrooms mobile',
-        imgColors: ohmyfoodColors,
-        altColors: 'ohmyfood openclassrooms colors',
-        imgIcons: ohmyfoodIcons,
-        altIcons: 'ohmyfood openclassrooms icons',
+        languages: [logoHtml, logoCss, logoSass],
+        picturesPres: [ohmyfoodDesk, ohmyfoodTab, ohmyfoodMobile],
+        graphicId: [ohmyfoodColors, ohmyfoodIcons],
         gitLink: 'https://github.com/CamilleLinan/Projet-3-Ohmyfood',
         webSite: 'https://camillelinan.github.io/Projet-3-Ohmyfood/'
     },
     {
         id: '01',
         name: 'Booki',
-        cover: booki,
+        cover: bookiCover,
         alt: 'cover booki openclassrooms',
         resume: 'Intégration de maquette en HTML et CSS',
         context: 'Booki est une petite entreprise proposant un outil de planification de vacances. Le site permet aux usagers de trouver des hébergements et des activités dans la ville de leur choix.',
         objectifs: 'Créer un prototype de site en intégrant la maquette conçue par le designer UI, en HTML et CSS. Le site doit bien sûr être responsive.',
-        html: logoHtml,
-        css: logoCss,
-        imgDesk: bookiDesk,
-        altDesk: 'cover booki openclassrooms desktop',
-        imgTab: bookiTab,
-        altTab: 'cover booki openclassrooms tablette',
-        imgMobile: bookiMobile,
-        altMobile: 'cover booki openclassrooms mobile',
-        imgColors: bookiColors,
-        altColors: 'booki openclassrooms colors',
-        imgIcons: bookiIcons,
-        altIcons: 'booki openclassrooms icons',
+        languages: [logoHtml, logoCss],
+        picturesPres: [bookiDesk, bookiTab, bookiMobile],
+        graphicId: [bookiColors, bookiIcons],
         gitLink: 'https://github.com/CamilleLinan/Projet-2-Booki',
         webSite: 'https://camillelinan.github.io/Projet-2-Booki/',
     }
